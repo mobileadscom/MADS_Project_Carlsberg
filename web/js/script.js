ea90f1930d01cd53499fe40451681d8f0a864804
Ninjoe <joequah1@gmail.com>
2017-05-15 18:33:16 +0800

    v1.01

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,7 @@
 var filesLists = [
         ['first-screen.png'],
         ['glass-empty.png', 'nozzle-empty.png', 'swipe-totap.png'],
-        ['nozzle-pour.png', 'glass-full.png', 'hand-icon.png', 'end-image.png', 'end-screen.png']
+        ['nozzle-pour.gif', 'glass-full.png', 'hand-icon.png', 'end-image.png', 'end-screen.png']
     ],
     listsCount = filesLists.length;
 
@@ -57,6 +57,7 @@ function id(str) {
 }
 
 function startAnimation() {
+    sound.currentTime = iOS ? 4 : 2;
     var start = false,
         ge = id("glass-empty"),
         ne = id("nozzle-empty"),
@@ -78,10 +79,70 @@ function startAnimation() {
         }
     }, 3200);
 
-    
 
-    window.addEventListener('touchend', pour);
-    window.addEventListener('mouseup', pour);
+
+    var hammertime = new Hammer(document.getElementById('first-screen'));
+    var swipe = new Hammer.Swipe()
+    hammertime.add(swipe)
+    hammertime.on('swipedown', pour);
+
+    document.getElementById('first-screen').addEventListener('touchstart', handleTouchStart, false);        
+    document.getElementById('first-screen').addEventListener('touchmove', handleTouchMove, false);
+    document.getElementById('first-screen').addEventListener('mousedown', handleTouchStart, false);        
+    document.getElementById('first-screen').addEventListener('mousemove', handleTouchMove, false);
+    var xDown = null;                                                        
+    var yDown = null;                                                        
+    function handleTouchStart(evt) {                                         
+        xDown = evt.clientX || evt.touches[0].clientX;                                      
+        yDown = evt.clientY || evt.touches[0].clientY;    
+
+            sound.play();
+            sound.currentTime = iOS ? 4 : 2;
+            setTimeout(function () {
+            if (!start) {
+                sound.pause();
+            }
+        }, 1000)    
+                                
+    };                                                
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        var xUp = evt.clientX || evt.touches[0].clientX;                                    
+        var yUp = evt.clientY || evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+        if(Math.abs( xDiff )+Math.abs( yDiff )>50){ //to deal with to short swipes
+
+        if ( Math.abs( xDiff ) <= Math.abs( yDiff ) ) {/*most significant*/
+            if ( yDiff <= 0 ) {
+                sound.play();
+                pour();
+            }                                                                 
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+        }
+    };
+
+
+    document.getElementById('first-screen').addEventListener('touchend', function (){        
+        sound.play();
+        setTimeout(function () {
+            if (!start) {
+                sound.pause();
+            }
+        }, 1000)
+    });
+
+
+
+    //document.getElementById('first-screen').addEventListener('touchend', pour);
+    //document.getElementById('first-screen').addEventListener('mouseup', pour);
 
     if(iOS) {
         document.getElementById('first-screen').addEventListener('click', pour);
@@ -89,28 +150,29 @@ function startAnimation() {
 
 
     function pour() {
+
+
         if (!start) {
             myApp.sdk.tracker('E', 'pour', 'pour');
 
             start = true;
-            sound.currentTime = iOS ? 4 : 2;
             sound.play();
             id("glass-full").clip = phone ? 'rect(0, 90px, 213px, 0)' : 'rect(0, 180px, 426px, 0)';
             var np = id("nozzle-pour");
-            np.height = phone ? '139px' : '309px';
+            np.height = phone ? '117px' : '309px';
             hi.display = '';
             st.transition = 'opacity 2s';
             st.opacity = 0;
 
             setTimeout(function () {
-                np.clip = phone ? 'rect(0, 75px, 139px, 0)' : 'rect(0, 152px, 309px, 0)';
+                np.clip = phone ? 'rect(0, 75px, 117px, 0)' : 'rect(0, 152px, 309px, 0)';
                 np.transitionProperty = 'clip';
                 np.transitionDuration = '1.8s';
 
                 requestAnimationFrame(function () {
-                    np.clip = phone ? 'rect(139px, 75px, 139px, 0)' : 'rect(309px, 152px, 309px, 0)';
+                   // np.clip = phone ? 'rect(117px, 75px, 117px, 0)' : 'rect(309px, 152px, 309px, 0)';
                 });
-            }, 2700);
+            }, 2600);
 
             var es = id("end-screen");
             var ei = id("end-image");
@@ -118,15 +180,16 @@ function startAnimation() {
             es.display = 'block';
 
             setTimeout(function () {
+                var url = 'https://www.probablythebest.com.my/#!smooth-draught';
                 ei.opacity = 1;
                 es.left = '0';
                 es.opacity = 1;
                 document.getElementById("end-image").onclick = function () {
-                    myApp.sdk.linkOpener('http://www.carlsberg.com');
+                    myApp.sdk.linkOpener(url);
                     myApp.sdk.tracker('CTR', 'landing', 'landing');
-                }
+                };
                 document.getElementById("end-screen").onclick = function () {
-                    myApp.sdk.linkOpener('http://www.carlsberg.com');
+                    myApp.sdk.linkOpener(url);
                     myApp.sdk.tracker('CTR', 'landing', 'landing');
                 }
             }, 6200);
